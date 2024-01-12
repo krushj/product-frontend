d7237cfbc22832d3e128320da772d763da4f28b6
krushna <krushna@Administrators-MacBook-Pro.local>
2024-01-12 18:34:05 +0530

    token actions

---
+++ src/app/auth/actions/token.actions.ts
@@ -0,0 +1,30 @@
+import { createAction, props } from "@ngrx/store";
+
+const getToken = createAction('[Token] GET', props<{username: string, password: string}>())
+const getTokenSucess = createAction('[Token] GET SUCCESS')
+const getTokenFailure = createAction('[Token] GET', props<{error: string}>())
+
+
+const loadToken = createAction('[Token] LOAD')
+const loadTokenSuccess = createAction('[TOKEN] LOAD SUCCESS')
+const loadTokenFailure = createAction('[TOKEN] LOAD FAILURE', props<{error: string}>())
+
+const removeToken = createAction('[TOKEN] REMOVE')
+const removeTokenSuccess = createAction('[TOKEN] REMOVE SUCCESS')
+const removeTokenFailure = createAction('[TOKEN] REMOVE FAILURE')
+
+
+export {
+    getToken,
+    getTokenSucess,
+    getTokenFailure,
+
+    
+    loadToken,
+    loadTokenSuccess,
+    loadTokenFailure,
+
+    removeToken,
+    removeTokenSuccess,
+    removeTokenFailure
+}

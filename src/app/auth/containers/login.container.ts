3fff33abb3eab833a6dd4fbd71b97a1e0be9c9da
krushna <krushna@Administrators-MacBook-Pro.local>
2024-01-12 18:26:33 +0530

    login container completed

---
+++ src/app/auth/containers/login.container.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectTokenError } from '../selectors/form.selector';
+import { getToken, loadToken } from '../actions/token.actions';
+
+@Component({
+  selector: 'login-container',
+  template: `
+    <app-login
+      [error]="error$ | async"
+      (formSubmitEmitter)="formSubmitted($event)"
+    />
+  `,
+})
+export class LoginContainerComponent implements OnInit {
+  
+
+  public error$: Observable<string>;
+
+  formSubmitted($event : {username: string, password : string}){
+    this.store$.dispatch(getToken({username: $event.username, password: $event.password}))
+  }
+  
+
+  constructor(private store$: Store) {
+    this.error$ = this.store$.select(selectTokenError);
+  }
+
+  ngOnInit() {
+    this.store$.dispatch(loadToken())
+  }
+}
